44b640ea0801b8817397a238b648450ebd3fed94
Arlandaren <bad-07@list.ru>
2024-08-31 03:07:36 +0900

    fixed: routes string

--- pkg/handlers/router.go
+++ pkg/handlers/router.go
@@ -11,8 +11,8 @@ func InitRoutes(r *gin.Engine, s *services.Services) {
 	{
 		api_router.Group("auth")
 		{
-			api_router.GET("token/:user_id", GenerateTokens(s))
-			api_router.POST("token/refresh", RefreshAccessToken(s))
+			api_router.GET("/token/:user_id", GenerateTokens(s))
+			api_router.POST("/token/refresh", RefreshAccessToken(s))
 		}
 	}
 
